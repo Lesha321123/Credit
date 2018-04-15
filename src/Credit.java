976c2f09c8ddcfa03782b9efebdcebce05ee317c
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-04-15 10:57:17 +0300

    Create Methods

--- src/Credit.java
+++ src/Credit.java
@@ -3,19 +3,32 @@ import java.util.Scanner;
 public class Credit {
 
     public static void main(String[] args) {
+
         System.out.println("Введите сумму кредита");
-        Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
+        int i = Enter();
+
 
         System.out.println("Введите процентную ставку");
-        Scanner sc2 = new Scanner(System.in);
-        double st = sc2.nextInt();
-        double mon = st / 12 + 5;
+        double mon = Monthly();
+
 
         System.out.println("Ежемесячный платеж");
         double com = i /100 * mon;
-
         System.out.println(com);
 
     }
+    //Методы
+    //Ввод данных
+    public static int Enter() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    //Процентная ставка
+    public static double Monthly() {
+        Scanner sc2 = new Scanner(System.in);
+        double st = sc2.nextInt();
+        return st / 12 + 5;
+    }
+
 }
