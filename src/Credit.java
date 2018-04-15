0e94e92969b8b477aaa9a3756dcb29e280706f8b
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-04-15 09:55:51 +0300

    correction

--- src/Credit.java
+++ src/Credit.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Credit {
   static void Enter(){
-        System.out.println("Введите сумму кредита");
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
     }
 
 
     public static void main(String[] args) {
-
+        System.out.println("Введите сумму кредита");
         Enter();
+        
     }
 }
