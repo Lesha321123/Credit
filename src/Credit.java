13444814351024b25eea644ef3092831d0b9d12c
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-04-15 10:39:12 +0300

    working programm

--- src/Credit.java
+++ src/Credit.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class Credit {
-  static void Enter(){
-        Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-    }
-
 
     public static void main(String[] args) {
         System.out.println("Введите сумму кредита");
-        Enter();
-        
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        System.out.println("Введите процентную ставку");
+        Scanner sc2 = new Scanner(System.in);
+        double st = sc2.nextInt();
+        double mon = st / 12 + 5;
+
+        System.out.println("Ежемесячный платеж");
+        double com = i /100 * mon;
+
+        System.out.println(com);
+
     }
 }
