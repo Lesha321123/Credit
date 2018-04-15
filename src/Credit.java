ecdec35e2717695cf9ac619a111e8f7e51075c07
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-04-15 09:51:51 +0300

    Create method Enter

---
+++ src/Credit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Credit {
+  static void Enter(){
+        System.out.println("Введите сумму кредита");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+    }
+
+
+    public static void main(String[] args) {
+
+        Enter();
+    }
+}
