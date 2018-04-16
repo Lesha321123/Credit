c8a5946e84498156fa8d458973db49fa316a5cf7
Lesha321123 <37781150+Lesha321123@users.noreply.github.com>
2018-04-16 23:23:11 +0300

    Revert "Comments"
    This reverts commit f12be1f4fafa670aab6acb220841bf01724ca80d.

--- src/Credit.java
+++ src/Credit.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 public class Credit {
 
     public static void main(String[] args) {
-        //Вводим сумму кредита
         System.out.println("Введите сумму кредита");
         double debt = Enter();
-        //Вводим процентную ставку
+
         System.out.println("Введите процентную ставку");
         double mon = Monthly();
         //Считаем задолженность и обязательный платеж
@@ -17,6 +16,15 @@ public class Credit {
             System.out.println("Обязательный платеж: " + Math.round(pay));
             debt = debt - pay;
             }
+
+        while (debt>150){
+          double  r = debt / 100 * mon;
+            System.out.println("Обязательный платеж: " + Math.round(r));
+            double n = debt - r;
+            System.out.println("Задолженность: " + Math.round(n));
+            debt = debt - r;
+        }
+
 
     }
     //Методы
